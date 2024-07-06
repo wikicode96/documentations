65c4a41d48be0d255e99f3b180fb0ba73fe8c3b9
Carlos Soler García <wikicode96@gmail.com>
2024-07-06 18:54:30 +0200

    project of spring security with db

---
+++ spring/spring-security/spring-security-db/src/main/java/com/github/wikicode96/spring_security_db/SpringSecurityWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.github.wikicode96.spring_security_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithDbApplication.class, args);
+	}
+
+}
---
+++ spring/spring-security/spring-security-db/src/test/java/com/github/wikicode96/spring_security_db/SpringSecurityWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wikicode96.spring_security_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
